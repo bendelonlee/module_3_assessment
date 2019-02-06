6ac20270c1de90508696ed3cb3b9425ea9a6487f
Ben Lee <bendelonlee@gmail.com>
2019-02-06 09:54:23 -0700

    Service test passing

--- app/services/nrel_fuel_station_service.rb
+++ app/services/nrel_fuel_station_service.rb
@@ -2,9 +2,7 @@ class NrelFuelStationService
   def stations(zip_code)
     get_json("nearest.json", zip_code)
   end
-
-# https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=BQHi4bhlF3e69MncBYp2Gys3zaiUrkM4OaiYAxZm&format=JSON&location=80203&fuel_type=LPG,ELEC&limit=10&radius=6.0
-
+  
   def get_json(path, zip_code)
     response = connection.get(path)
     JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
