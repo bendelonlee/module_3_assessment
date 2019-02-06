473a9a261b8e5bd6d8f252e996cd07dab2388d8e
Ben Lee <bendelonlee@gmail.com>
2019-02-06 10:19:04 -0700

    Gets basic feature test passing

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,5 +1,10 @@
 class Station
+  attr_reader :name, :address, :fuel_types, :distance, :access_times
   def initialize(data)
-    @data = data
+    @name = data[:station_name]
+    @address = data[:street_address]
+    @fuel_types = [data[:fuel_type_code]]
+    @distance = data[:distance]
+    @access_times = data[:access_days_time]
   end
 end
--- spec/poros/station_spec.rb
+++ spec/poros/station_spec.rb
@@ -4,4 +4,20 @@ describe Station do
     station = Station.new({})
     expect(station).to be_a(Station)
   end
+  it 'has attributes' do
+    args = {
+            name: "something",
+            address: "something",
+            fuel_types: "something",
+            distance: "something",
+            accesstimes: "something"
+          }
+    station = Station.new(args)
+    expect(station.name).to eq(args[:name])
+    expect(station.address).to eq(args[:address])
+    expect(station.fuel_types).to eq(args[:fuel_types])
+    expect(station.distance).to eq(args[:distance])
+    expect(station.accesstimes).to eq(args[:accesstimes])
+  end
+
 end
